ae37d6cd6490733f248f95268d49d2572db57464
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-04-04 23:07:55 +0500

    updates

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <Container className='text-center'>
+      <p>copyright &copy; 2021</p>
+    </Container>
+  );
+};
+
+export default Footer;
