434d1fcb28e44b1d8de0b71143053fa297ceea12
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-04-04 23:34:05 +0500

    updates

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 
 // DUMMY DATA
 import products from "../dummyData/products";
 
 const HomeScreen = () => {
-  return <div></div>;
+  return (
+    <>
+      <h1>Latest Product</h1>
+      <Row>
+        {products.map((item) => (
+          <Col sm={12} md={6}>
+            <h3>{products.name}</h3>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default HomeScreen;
