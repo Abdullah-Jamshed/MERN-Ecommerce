dac877c425f6e330fb7efa172f29c887f71c1f76
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-04-07 15:46:57 +0500

    updates

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,8 @@
-console.log("server")
+const express = require("express")
+require("dotenv").config({ path: "../.env" });
+
+
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("port", process.env.PORT || 3000);
