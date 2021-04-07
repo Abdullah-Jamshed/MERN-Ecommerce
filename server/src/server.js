9d3791d1837fbd41bf2146803f3e07c2d14c3807
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-04-07 15:51:26 +0500

    updates

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,11 @@
-const express = require("express")
+const express = require("express");
 require("dotenv").config({ path: "../.env" });
 
-
-const app = express()
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("port", process.env.PORT || 3000);
+
+app.listen(app.get("port"), () => {
+  console.log(`Express Started on: http://localhost:${app.get("port")}`);
+});
