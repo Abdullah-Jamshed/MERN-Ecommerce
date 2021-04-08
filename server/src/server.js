68fb9a32e8ee6199c2e3a2699d200980388d1508
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-04-08 20:40:29 +0500

    updates

--- server/src/server.js
+++ server/src/server.js
@@ -7,5 +7,5 @@ app.use(express.urlencoded({ extended: true }));
 app.set("port", process.env.PORT || 3000);
 
 app.listen(app.get("port"), () => {
-  console.log(`Express Started on: http://localhost:${app.get("port")}`);
+  // console.log(`Express Started on: http://localhost:${app.get("port")}`);
 });
