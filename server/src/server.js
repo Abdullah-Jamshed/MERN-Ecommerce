a3f15321eb26495e6518e063f25977e5c498426e
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-04-10 16:43:36 +0500

    updates

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config();
+
+console.log(process.env.PORT);
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3001);
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello world" });
+});
 
 app.listen(app.get("port"), () => {
   console.log(`Express Started on: http://localhost:${app.get("port")}`);
 });
-
