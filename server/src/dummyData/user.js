c40e41f003651f2fafb9b682a84a6feacf74adf2
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-04-11 00:20:15 +0500

    updates

---
+++ server/src/dummyData/user.js
@@ -0,0 +1,18 @@
+const user = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: "password",
+    isAdmin: true,
+  },
+  {
+    name: "User 1",
+    email: "user1@example.com",
+    password: "password",
+  },
+  {
+    name: "User 2",
+    email: "user2@example.com",
+    password: "password",
+  },
+];
