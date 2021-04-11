4758d56f3af36bf3abbf49e1bb80c7409346089e
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-04-12 03:14:13 +0500

    updates

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,4 +1,11 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+// UI LIBRARY COMPONENT
+import { Col, Row } from "react-bootstrap";
+
+// COMPONENTS
+import Message from "../components/Message";
 
 // REDUX
 import { useDispatch, useSelector } from "react-redux";
@@ -14,13 +21,26 @@ const CartScreen = ({ match, location, history }) => {
   const qty = location.search ? Number(location.search.split("=")[1]) : 1;
 
   useEffect(() => {
-    dispatch(addCartItem(match.params.id, qty));
+    if (match.params.id) dispatch(addCartItem(match.params.id, qty));
   }, [dispatch, match, qty]);
 
   return (
-    <div>
-      <h1>Cart Screen</h1>
-    </div>
+    <>
+      <Row className='text-center p-4'>
+        <Col md={8}>
+          <h1>Shopping Cart</h1>
+          {cartItems.length === 0 ? (
+            <Message>
+              Your Cart is Empty <Link to='/'>Go Back</Link>
+            </Message>
+          ) : (
+            "items"
+          )}
+        </Col>
+        <Col md={2}></Col>
+        <Col md={2}></Col>
+      </Row>
+    </>
   );
 };
 
