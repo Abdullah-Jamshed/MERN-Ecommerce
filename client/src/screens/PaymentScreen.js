61f2d7f2d54de3e9a6cf7bb9ee53d961d1da26b6
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-04-17 22:32:38 +0500

    updates

--- client/src/screens/PaymentScreen.js
+++ client/src/screens/PaymentScreen.js
@@ -1,70 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-// UI LIBRARY COMPONENT
-import { Container, Button, Form, Row, Col, Spinner } from "react-bootstrap";
-
-//  COMPONENT
-import FormContainer from "../components/FormContainer";
-import CheckoutSteps from "../components/CheckoutSteps";
-import Message from "../components/Message";
-
-// REDUX
-import { useDispatch, useSelector } from "react-redux";
-
-const PaymentScreen = ({ history, location }) => {
-  // REDUX STATE HOOK
-  const { shippingAddress } = useSelector((state) => state.shippingReducer);
-
-  // REDUX DISPATCH HOOK
-  //   const dispatch = useDispatch();
-
-  // HANDLER FUNCTIONS
-
-  // LIFECYCLE
-
-  //   useEffect(() => {
-  //     console.log("payment useEffect ==>>> ", shippingAddress);
-
-  //     if (shippingAddress) {
-  //       //   console.log(shippingAddress);
-  //       // setForm(shippingAddress);
-  //     }
-  //   }, [shippingAddress]);
-
-  return (
-    <Container className='py-4'>
-      <FormContainer>
-        <CheckoutSteps step1 step2 />
-        <h1>Shipping</h1>
-        {/* {(errorMessage || errorMsg) && <Message variant='danger'>{errorMessage || errorMsg}</Message>} */}
-        {/* <Form onSubmit={submitHandler}>
-          <Form.Group controlId='address'>
-            <Form.Label>Address : </Form.Label>
-            <Form.Control type='address' placeholder='address' value={form.address} name='address' onChange={formHandler}></Form.Control>
-          </Form.Group>
-          <Form.Group controlId='city'>
-            <Form.Label>City </Form.Label>
-            <Form.Control type='city' placeholder='city' value={form.city} name='city' onChange={formHandler}></Form.Control>
-          </Form.Group>
-          <Form.Group controlId='postalCode'>
-            <Form.Label>Postal Code : </Form.Label>
-            <Form.Control type='postalCode' placeholder='postalCode' value={form.postalCode} name='postalCode' onChange={formHandler}></Form.Control>
-          </Form.Group>
-          <Form.Group controlId='country'>
-            <Form.Label>Country : </Form.Label>
-            <Form.Control type='text' placeholder='country' value={form.country} name='country' onChange={formHandler}></Form.Control>
-          </Form.Group>
-
-          <Button
-            type='submit'
-            className='mt-2 btn-block'
-            disabled={form.address === "" || form.city === "" || form.postalCode === "" || form.country === ""}>
-            Continue
-          </Button>
-        </Form> */}
-      </FormContainer>
-    </Container>
-  );
+const PaymentScreen = () => {
+  return <div>payment</div>;
 };
 
 export default PaymentScreen;
--- client/src/store/actions/shippingActions.js
+++ client/src/store/actions/shippingActions.js
@@ -1,6 +1,6 @@
 const saveShippingAddress = (shippingAddress) => {
   return (dispatch) => {
-    dispatch({ type: "SAVE_SHIPPING_ADDRESS", payload: shippingAddress });
+    if (shippingAddress) dispatch({ type: "SAVE_SHIPPING_ADDRESS", payload: shippingAddress });
     localStorage.setItem("shipping_address", JSON.stringify(shippingAddress));
   };
 };
