a824a145d60dacaef60715aacac8535011cf6381
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-05-09 08:52:52 +0500

    card text ellipsis

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -7,6 +7,15 @@ import { Card } from "react-bootstrap";
 //  COMPONENTS
 import Rating from "./Rating";
 
+// STYLES
+const styles = {
+  textStyle: {
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  },
+};
+
 const Product = ({ product }) => {
   return (
     <Card style={{ width: "100%" }} className='my-2 mx-auto'>
@@ -14,7 +23,7 @@ const Product = ({ product }) => {
         <Card.Img variant='top' src={product.image} />
       </Link>
       <Card.Body className='text-left'>
-        <Card.Title>{product.name}</Card.Title>
+        <Card.Title style={styles.textStyle}>{product.name}</Card.Title>
         <Card.Text as='div'>
           <Rating rating={product.rating} reviews={product.numReviews} />
         </Card.Text>
